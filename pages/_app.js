63ab98e20ce1a7e84fc125b017c0b04aed98e930
LucaB <luca.bertoldi89@gmail.com>
2022-03-24 11:50:53 +0100

    added auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
-
+import Amplify from 'aws-amplify';
+import config from '../src/aws-exports';
+Amplify.configure(config);
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default withAuthenticator(MyApp);
